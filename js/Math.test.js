883afc955018d4505d99501eaf33ed9a976e768d
Rimantas <belauzas@yahoo.com>
2020-12-25 00:13:46 +0200

    code quality fix

--- js/Math.test.js
+++ js/Math.test.js
@@ -7,7 +7,7 @@ describe("Sum function is working", () => {
     test("adds -1 + -2 to equal -3", () => {
         expect(Math.sum(-1, -2)).toBe(-3);
     });
-})
+});
 
 describe("Minus function is working", () => {
     test("adds 1 - 2 to equal -1", () => {
@@ -16,7 +16,7 @@ describe("Minus function is working", () => {
     test("adds -1 - -2 to equal 1", () => {
         expect(Math.minus(-1, -2)).toBe(1);
     });
-})
+});
 
 describe("Multiply function is working", () => {
     test("adds 1 * 2 to equal 2", () => {
@@ -25,7 +25,7 @@ describe("Multiply function is working", () => {
     test("adds -1 * -2 to equal 2", () => {
         expect(Math.multiply(-1, -2)).toBe(2);
     });
-})
+});
 
 describe("Divide function is working", () => {
     test("adds 1 / 2 to equal 0.5", () => {
@@ -34,4 +34,4 @@ describe("Divide function is working", () => {
     test("adds -1 / -2 to equal 0.5", () => {
         expect(Math.divide(-1, -2)).toBe(0.5);
     });
-})
+});
