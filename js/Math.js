5d22118c18eaa5e2ed7dee9d6801654eea30e7be
Rimantas <belauzas@yahoo.com>
2020-12-25 01:53:17 +0200

    pretty html update

--- js/Math.js
+++ js/Math.js
@@ -1,19 +1,19 @@
 class Math {
     static sum(a, b) {
-        return a + b
+        return a + b;
     }
 
     static minus(a, b) {
-        return a - b
+        return a - b;
     }
 
     static multiply(a, b) {
-        return a * b
+        return a * b;
     }
 
     static divide(a, b) {
-        return a / b
+        return a / b;
     }
 }
 
-module.exports = Math
+module.exports = Math;
