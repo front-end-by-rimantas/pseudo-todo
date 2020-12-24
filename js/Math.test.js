dc774004252fc402858cd1da971dc60fd59448c6
Rimantas <belauzas@yahoo.com>
2020-12-25 01:32:42 +0200

    prety eslint

--- js/Math.test.js
+++ js/Math.test.js
@@ -1,37 +1,37 @@
-const Math = require("./Math");
+const Math = require("./Math")
 
 describe("Sum function is working", () => {
     test("adds 1 + 2 to equal 3", () => {
-        expect(Math.sum(1, 2)).toBe(3);
-    });
-    test("adds -1 + -2 to equal -3", () => {
-        expect(Math.sum(-1, -2)).toBe(-3);
-    });
-});
+        expect(Math.sum(1, 2)).toBe(3)
+    })
+    test('adds -1 + -2 to equal -3', () => {
+        expect(Math.sum(-1, -2)).toBe(-3)
+    })
+})
 
 describe("Minus function is working", () => {
     test("adds 1 - 2 to equal -1", () => {
-        expect(Math.minus(1, 2)).toBe(-1);
-    });
+        expect(Math.minus(1, 2)).toBe(-1)
+    })
     test("adds -1 - -2 to equal 1", () => {
-        expect(Math.minus(-1, -2)).toBe(1);
-    });
-});
+        expect(Math.minus(-1, -2)).toBe(1)
+    })
+})
 
 describe("Multiply function is working", () => {
     test("adds 1 * 2 to equal 2", () => {
-        expect(Math.multiply(1, 2)).toBe(2);
-    });
+        expect(Math.multiply(1, 2)).toBe(2)
+    })
     test("adds -1 * -2 to equal 2", () => {
-        expect(Math.multiply(-1, -2)).toBe(2);
-    });
-});
+        expect(Math.multiply(-1, -2)).toBe(2)
+    })
+})
 
 describe("Divide function is working", () => {
     test("adds 1 / 2 to equal 0.5", () => {
-        expect(Math.divide(1, 2)).toBe(0.5);
-    });
+        expect(Math.divide(1, 2)).toBe(0.5)
+    })
     test("adds -1 / -2 to equal 0.5", () => {
-        expect(Math.divide(-1, -2)).toBe(0.5);
-    });
-});
+        expect(Math.divide(-1, -2)).toBe(0.5)
+    })
+})
